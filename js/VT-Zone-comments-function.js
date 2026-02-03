cc2bb30001c77f2ef1a188c36630449ef52e53ad
Vũ Trường <54496690+vutruongvn@users.noreply.github.com>
2026-02-03 10:11:30 +0700

    Update VT-Zone-comments-function.js

--- js/VT-Zone-comments-function.js
+++ js/VT-Zone-comments-function.js
@@ -177,7 +177,7 @@ function initCommentLogic() {
                 const count = replyArea.querySelectorAll('.comment-thread > ol > li').length;
                 const viewBtn = document.createElement('div');
                 viewBtn.className = 'view-replies small ms-5 pb-2';
-                viewBtn.innerHTML = `<span class="has-hover"><i class="fa-regular fa-angle-down"></i> ${count} phản hồi</span>`;
+                viewBtn.innerHTML = `<a class="viewRepliesBtn small fw-medium"><i class="fa-regular fa-angle-down"></i> ${count} phản hồi</a>`;
                 replyArea.before(viewBtn);
             }
         });
@@ -241,4 +241,5 @@ if (document.readyState === 'loading') {
 } else {
     initCommentLogic();
 }
+
 
