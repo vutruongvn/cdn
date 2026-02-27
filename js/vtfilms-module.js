48e4982117e2a5e39027aed132a3419b562552cf
Vũ Trường <54496690+vutruongvn@users.noreply.github.com>
2026-02-27 13:37:40 +0700

    Update vtfilms-module.js
    xóa uid dNkYpISZzgdpoJ4fVQUzWAFWgVw1

--- js/vtfilms-module.js
+++ js/vtfilms-module.js
@@ -77,7 +77,6 @@ const VTFilms_DEBUG = true;
 // Admin UIDs — thêm UID vào đây để cấp quyền admin. Mọi UID khác = "user".
 const VTFilms_ADMIN_UIDS = [
     'KU6FC2SAsmaE8qIu4EGU9J422On1', // admin
-	'dNkYpISZzgdpoJ4fVQUzWAFWgVw1', // truongdz2069
 ];
 
 // ── Storage Keys ──
